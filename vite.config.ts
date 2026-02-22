fd058c40e4e6b043754f882f0508785f1f5eb564
MaWanBus <on99plk83@gmail.com>
2026-02-22 12:02:08 +0800

    Update vite.config.ts
    Signed-off-by: MaWanBus <on99plk83@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // This path must match your repository name exactly
+      base: '/MaWanBusv2-Beta/',
       server: {
         port: 3000,
         host: '0.0.0.0',
